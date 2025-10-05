2a9eaaae42ebc0a76fdd17bac76d92f8ae032871
Jonathan Thralow <jon@thralow.com>
2025-10-05 10:41:10 -0500

    Update and rename vite.config.js to vite.config.mjs

---
+++ vite.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@tests': path.resolve(__dirname, './src/tests')
+    },
+  },
+})
